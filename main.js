eebe27241c87661ab199015405d7c9d4c37aa54b
Érick <eierickdasilva@gmail.com>
2024-02-16 09:00:01 -0300

    Feature: adding electron and electron builder for app desktop linux

--- main.js
+++ main.js
@@ -1,94 +1,23 @@
-const passwordDisplay = document.querySelector(".password-display");
-const reloadBtn = document.querySelector(".reload-img");
-const copyBtn = document.querySelector(".copy-btn");
-const rangeDisplay = document.querySelector(".range-display");
-const inputRange = document.querySelector(".input-range");
-const characterUsed = document.querySelectorAll(".character-used");
+const { app, BrowserWindow } = require("electron");
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
-const alphabetUpperCase = alphabet.toLocaleUpperCase();
-const numbers = "0123456789";
-const symbols = "!@#$%&*_-";
+const createWindow = () => {
+    const win = new BrowserWindow({
+        windth: 800,
+        height: 630,
+        minWidth: 440,
+        minHeight: 630,
+    });
+    win.loadFile("index.html");
+    win.setMenu(null);
+}
 
-const rangeValue = 8;
-inputRange.value = rangeValue;
-rangeDisplay.innerHTML = rangeValue;
-
-let currentAngle = false;
-
-reloadBtn.addEventListener("click", () => {
-    currentAngle += 360;
-    reloadBtn.style.transform = `rotate(${currentAngle}deg)`;
-    generatePassword(document.querySelectorAll(".selected"));
-});
-
-copyBtn.addEventListener("click", () => {
-    navigator.clipboard.writeText(passwordDisplay.innerHTML).then(() => {
-        if (copyBtn.innerHTML === "Copied!") return;
-        copied();
+app.whenReady().then(() => {
+    createWindow();
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
 });
 
-inputRange.addEventListener("change", () => {
-    rangeDisplay.innerHTML = inputRange.value;
-    generatePassword(document.querySelectorAll(".selected"));
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
 });
-
-characterUsed.forEach((character) => {
-    character.addEventListener("click", () => {
-        const className = character.classList;
-        className.toggle("selected");
-        if (document.querySelectorAll(".selected").length === 0) className.toggle("selected");
-        generatePassword(document.querySelectorAll(".selected"));
-    });
-});
-
-generatePassword(document.querySelectorAll(".selected"));
-
-function generatePassword(selectedsCharacters) {
-    let password = "";
-    let AllcharacterListUsed = "";
-
-    selectedsCharacters.forEach((character) => {
-        switch (character.id) {
-            case "character-lowercase":
-                AllcharacterListUsed += alphabet;
-                break;
-            case "character-uppercase": 
-                AllcharacterListUsed += alphabetUpperCase;
-                break;
-            case "character-numbers": 
-                AllcharacterListUsed += numbers;
-                break;
-            case "character-symbols":
-                AllcharacterListUsed += symbols;
-                break;
-            default: 
-                AllcharacterListUsed.push(alphabet, alphabetUpperCase, numbers, symbols);
-        }
-    });
-
-    for (let i = 0; i < inputRange.value; i++) {
-        const num = getRandom(0, AllcharacterListUsed.length);
-        password += AllcharacterListUsed[num];
-    }
-
-    passwordDisplay.innerHTML = password;
-}
-
-function getRandom(min, max) {
-    const numRandom = Math.floor(Math.random() * (max - min) + min);
-    return numRandom;
-}
-
-function copied() {
-    copyBtn.innerHTML = "Copied!";
-    copyBtn.style.background = "gray";
-    copyBtn.style.cursor = "default";
-
-    setTimeout(() => {
-        copyBtn.innerHTML = "copy";
-        copyBtn.style.background = "var(--primary-color)";
-        copyBtn.style.cursor = "pointer";
-    }, 2000);
-}
